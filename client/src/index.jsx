731e30693918d8bcfa9556b775d2d6324fbf17ed
rainealexander <raine@rainealexander.dev>
2021-09-04 15:39:26 -0500

    Test app now rendering client from server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <h1>Hello from React!!!</h1>
-    )
+    );
   }
 
 }
+
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('app')
 );
