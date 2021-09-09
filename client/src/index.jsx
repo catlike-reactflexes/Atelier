cbeeb0db742dfd9012c1ec3c31441c81d489c138
rainealexander <raine@rainealexander.dev>
2021-09-08 23:28:01 -0500

    Resolve merge conflicts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Overview from './components/overview/overview.jsx';
+import QaMain from './components/qa/QaMain.jsx';
 
 class App extends React.Component {
   constructor(props) {
--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-app.use('/', express.static(path.join(__dirname, '/client/dist')));
+
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
