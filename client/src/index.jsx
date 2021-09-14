34acae93de3ee2f3fb2136a0555d7cbd4f4096be
rainealexander <raine@rainealexander.dev>
2021-09-13 23:32:40 -0500

    Research on passing data to server with axios

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: null
+      productId: 47421
     }
   }
 
@@ -17,7 +17,7 @@ class App extends React.Component {
     return (
       <div>
         <div>Header Placeholder</div>
-        <Overview />
+        <Overview id={this.state.productId}/>
         <RelatedProducts />
         <QaMain />
       </div>
--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+app.get('/product', (req, res) => {
+  let data = req.body;
+  console.log('req.body: ', req.body);
+  console.log('req.data: ', req.data);
+  console.log('req.query.id: ', req.query.id);
 });
 
 app.listen(port, () => {
