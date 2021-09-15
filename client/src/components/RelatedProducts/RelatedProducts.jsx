166459dd37a685839620f9c74f062799bca50ece
Anna <annamp727@gmail.com>
2021-09-15 18:09:54 -0500

    Rendering related product data on the page

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -9,48 +9,7 @@ class RelatedProducts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      relatedProductsData: [
-        {
-          "id": 1,
-          "name": "Camo Onesie",
-          "slogan": "Blend in to your crowd",
-          "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
-          "category": "Jackets",
-          "default_price": "140"
-        },
-        {
-          "id": 2,
-          "name": "Bright Future Sunglasses",
-          "slogan": "You've got to wear shades",
-          "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
-          "category": "Accessories",
-          "default_price": "69"
-        },
-        {
-          "id": 1,
-          "name": "Camo Onesie",
-          "slogan": "Blend in to your crowd",
-          "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
-          "category": "Jackets",
-          "default_price": "140"
-        },
-        {
-          "id": 2,
-          "name": "Bright Future Sunglasses",
-          "slogan": "You've got to wear shades",
-          "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
-          "category": "Accessories",
-          "default_price": "69"
-        },
-        {
-          "id": 3,
-          "name": "Morning Joggers",
-          "slogan": "Make yourself a morning person",
-          "description": "Whether you're a morning person or not. Whether you're gym bound or not. Everyone looks good in joggers.",
-          "category": "Pants",
-          "default_price": "40"
-        }
-      ],
+      relatedProductsData: [],
       yourOutfitData: [],
       defaultProductId: 47421
     }
@@ -67,6 +26,9 @@ class RelatedProducts extends React.Component {
     })
       .then((relatedProductsData) => {
         console.log('success getting related products data in related products client index: ', relatedProductsData.data);
+        this.setState({
+          relatedProductsData: relatedProductsData.data
+        })
       })
       .catch((error) => {
         console.log('error getting related products data in related products client index: ', error);
