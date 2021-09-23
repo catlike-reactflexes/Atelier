e2ed53ed17c9aaaf6f46595220c3e82e7ee737e2
Trevor <trevorhackreactor@Gmail.com>
2021-09-22 20:01:29 -0700

    fixed problem with api requests

--- client/src/components/reviews/Review.jsx
+++ client/src/components/reviews/Review.jsx
@@ -5,7 +5,8 @@ const Review = (props) => {
   // console.log(props.review)
 
 let markReviewAsHelpful = (reviewID) => {
-  axios.put('/helpful', {
+  console.log('review id in helpful', reviewID)
+  axios.get('/helpful', {
     params: {
       review_id: reviewID
     }
@@ -19,11 +20,26 @@ let markReviewAsHelpful = (reviewID) => {
   })
 }
 
+let reportReview = (reviewID) => {
+  axios.get('/report', {
+    params: {
+      review_id: reviewID
+    }
+  })
+  .then(response => {
+    console.log('report put response', response)
+  })
+  .catch(error => {
+    console.log('report put error', error)
+    throw error
+  })
+}
   return (
     <div className='review'>
       <div className='reviewer_name'>{props.review.reviewer_name}</div>
       <div className='review_body'>{props.review.body}</div>
-      <button className='helpfulBtn' onClick = {markReviewAsHelpful(props.review.review_id)}>helpful?</button>
+      <button className='helpfulBtn' >helpful?</button>
+      <button className='reportBtn' >report</button>
     </div>
   )
 }
