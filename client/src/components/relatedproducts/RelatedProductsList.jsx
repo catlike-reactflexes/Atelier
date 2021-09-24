d096315d5286fc1fd25dcc784addb48a499b7f5b
Anna <annamp727@gmail.com>
2021-09-23 19:21:37 -0500

    [WIP] Added dependencies for chevron icons and began implementing card carousel

--- client/src/components/relatedproducts/RelatedProductsList.jsx
+++ client/src/components/relatedproducts/RelatedProductsList.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import RelatedProductsCard from './RelatedProductsCard.jsx';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 class RelatedProductsList extends React.Component {
   constructor(props) {
     super(props);
+    this.transformLeft = this.transformLeft.bind(this);
+    this.transformRight = this.transformRight.bind(this);
   }
 
+  transformLeft() {
+    // transform: "translateX-(25%)"
+    console.log('left chevron clicked')
+    document.getElementById("relatedProductCards").style.transform = "translateX(-25%)";
+  }
+
+  transformRight() {
+    // transform: "translateX(25%)"
+    console.log('right chevron clicked')
+    document.getElementById("relatedProductCards").style.transform = "translateX(25%)";
+  }
 
   render() {
     for (var i = 0; i < this.props.productData.length; i++) {
@@ -18,9 +32,13 @@ class RelatedProductsList extends React.Component {
         productFeatures={item.features} />
     })
     return (
-      <ul id="relatedProductCards">
-        {productItems}
-      </ul>
+      <div id="cardContainer">
+        <FaChevronLeft onClick={this.transformLeft} />
+        <ul id="relatedProductCards">
+          {productItems}
+        </ul>
+        <FaChevronRight onClick={this.transformRight} />
+      </div>
     )
   }
 }
