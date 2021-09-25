136f6108555f58b6b302bbeed04e5ec6e5161dd2
Trevor <trevorhackreactor@Gmail.com>
2021-09-25 12:03:32 -0700

    Added trackers

--- client/src/components/reviews/NewReview.jsx
+++ client/src/components/reviews/NewReview.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ClickTracker from '../trackInteractions/ClickTracker.jsx';
 
-const NewReview = () => {
+const NewReview = (props) => {
   return (
     <div className='buttons'>
-      <button className='newReview' type="button" onClick=>Add a Review</button>
-      <button className='moreReviews' type="button">More Reviews</button>
+      <button className='newReview' type="button" onClick= {() => {props.postTrackInteractions('new review button', 'reviews')}}>Add a Review</button>
+      <button className='moreReviews' type="button" onClick={() => {props.postTrackInteractions('load more reviews button', 'reviews')}}>More Reviews</button>
     </div>
   );
 };
