55230382fc8daf807ee97cd6b37b5e2a3243bab2
rainealexander <raine@rainealexander.dev>
2021-09-25 12:54:30 -0500

    Fixed star display

--- client/src/components/overview/productDetails.jsx
+++ client/src/components/overview/productDetails.jsx
@@ -4,7 +4,16 @@ const ProductDetails = (props) => {
   return (
     props.loaded ?
     <div id="detailsContainer" data-testid="overview-details">
-      <span>***** <a href="#">Read all reviews</a></span>
+      <div className={'overviewStars'}>
+        <div>
+          <span className={'fullStar'}></span>
+          <span className={'fullStar'}></span>
+          <span className={'fullStar'}></span>
+          <span className={'fullStar'}></span>
+          <span className={'fullStar'}></span>
+        </div>
+        <a href="#">Read all reviews</a>
+      </div>
       <span id="po-category">{props.category.toUpperCase()}</span>
       <h1 id="po-name">{props.name}</h1>
       <span>${props.price}</span>
