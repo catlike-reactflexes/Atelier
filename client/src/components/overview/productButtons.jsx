22da9a6a31b846341ad3b0cc865f4baccb759dfb
rainealexander <raine@rainealexander.dev>
2021-09-27 21:02:17 -0500

    Added per-size quantity on select

--- client/src/components/overview/productButtons.jsx
+++ client/src/components/overview/productButtons.jsx
@@ -6,22 +6,35 @@ class ProductButtons extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: this.props.selected
+      selected: this.props.selected,
+      quantity: 0
     }
     this.getSizes = this.getSizes.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   getSizes(skus) {
     let sizes = Object.keys(skus).map((key, index) => {
-      return <option id={key} key={key} data-quantity={skus[key].quantity}>{skus[key].size}</option>
+      return <option id={key} key={key} data-qty={skus[key].quantity} value={skus[key].size}>{skus[key].size}</option>
     })
-    sizes.unshift(<option value="" disabled selected>SELECT SIZE</option>);
-    return <select onClick={() => {this.props.postTrackInteractions('Select Size', 'Product Buttons')}} id="sizeSelect">{sizes}
+    sizes.unshift(<option key={0} value="SELECT SIZE" disabled>SELECT SIZE</option>);
+    return <select onClick={() => {this.props.postTrackInteractions('Select Size', 'Product Buttons')}} id="sizeSelect" onChange={this.handleChange} defaultValue={'SELECT SIZE'}>{sizes}
     </select>;
   }
 
-  displayQty(sku) {
+  handleChange(event) {
+    // get quantity available for selected size
+    this.setState({ quantity: event.target.options[event.target.selectedIndex].dataset.qty });
+  }
 
+  displayQty(qty) {
+    const MAX_QTY = 15;
+    qty = qty < MAX_QTY ? qty : MAX_QTY;
+    let amounts = [];
+    for (let i = 0; i <= qty; i++) {
+      amounts.push(<option key={i} value={i}>{i}</option>)
+    }
+    return <select id='qtySelect' defaultValue='0'>{amounts}</select>
   }
 
   render() {
@@ -30,11 +43,9 @@ class ProductButtons extends React.Component {
         {this.props.loaded ?
           <div>
             {this.getSizes(this.props.selected.skus)}
-            <select id="qtySelect">
-              <option>1</option>
-            </select>
+            {this.displayQty(this.state.quantity)}
             <button id="bagBtn" onClick={() => {this.props.postTrackInteractions('Add to bag', 'Product Buttons')}}>ADD TO BAG</button>
-            <button id="favBtn" onClick={() => {this.props.favoriteItem}} >*</button>
+            <button id="favBtn" onClick={this.props.favoriteItem} >*</button>
           </div>
         : <div>Loading Data</div>}
       </div>
