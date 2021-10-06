71524bd18776d3c9e46eb0a2013358aeea320473
rainealexander <raine@rainealexander.dev>
2021-10-05 21:15:27 -0500

    Remove zoom on exiting expanded view, styling and minor updates

--- client/src/components/overview/productDetails.jsx
+++ client/src/components/overview/productDetails.jsx
@@ -27,8 +27,6 @@ class ProductDetails extends React.Component {
     }
     if (remainder > 0) {
       let quarter = (Math.round(remainder * 4) / 4).toFixed(2);
-      console.log(`remainder: ${remainder}, quarter: ${quarter}`);
-      console.log('class quarter: ', classes[quarter])
       result.push(classes[quarter]);
       counter++;
     }
