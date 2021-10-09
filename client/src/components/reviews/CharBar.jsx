50ba386bc710e8ee45764f3f32f6c32fde278fe5
Trevor <trevorhackreactor@Gmail.com>
2021-10-08 18:31:47 -0700

    Fixed char bar

--- client/src/components/reviews/CharBar.jsx
+++ client/src/components/reviews/CharBar.jsx
@@ -11,13 +11,14 @@ class CharBar extends React.Component {
 
   render() {
     const options = this.state.options
+    console.log('options at 2', options[2])
     return(
       <React.Fragment>
         <div className='charBarCharacteristic'>{this.props.characteristic}:</div>
         <div className='charBarContainer'>
         {options.map((option) => <div key={option} style={{ width: '95%', height: '10px', backgroundColor: 'gray' }} className={`charBar${options.indexOf(option)}`} />)}
         <div className='char0'>{options[0]}</div>
-        <div className='char4'>{options[4]}</div>
+        <div className='char4'>{options[2]}</div>
         <FaCaretUp className='caret' style={{
           marginLeft: `${parseInt(this.props.point - 1, 10).toFixed(2) * 23}%`,
         }}/>
